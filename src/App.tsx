9b0bbca9f61af2c619c4ceed643fe4563b6869b1
Jean Fourest <jmfourest@gmail.com>
2024-10-09 21:41:05 +0200

    checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Checkbox from "./components/checkbox/Checkbox";
 
 function App() {
+  const [values, setValues] = useState<(string | number)[]>([]);
+
+  console.log(values);
 
   return (
     <>
-      
+      <Checkbox
+        group={[
+          { label: "All", value: "all", checked: false, checkAll: true },
+          { label: "Number", value: 6, checked: false, checkAll: false },
+          { label: "Hello", value: "hello", checked: false, checkAll: false },
+          { label: "Baka", value: "baka", checked: false, checkAll: false },
+        ]}
+        onChange={(e) => setValues(e)}
+        className="checkbox"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/checkbox/Checkbox.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import "./Checkbox.css";
+
+type Checkbox = {
+  label: string;
+  value: string | number;
+  checked: boolean;
+  checkAll: boolean;
+};
+
+type CheckboxProps = {
+  group: Checkbox[];
+  onChange: (newValue: (string | number)[]) => void;
+  className?: string;
+};
+
+function Checkbox({
+  group,
+  onChange,
+  className = "checkbox-vertical",
+}: CheckboxProps) {
+  const [checkboxes, setCheckboxes] = useState(group);
+  const handleChange = (index: number) => {
+    // set the new group of checkboxes
+    const newGroup = [...checkboxes];
+    newGroup[index].checked = !newGroup[index].checked;
+
+    // if the checkbox is checkAll, check all the checkboxes
+    if (newGroup[index].checkAll) {
+      newGroup.forEach((checkbox) => {
+        checkbox.checked = newGroup[index].checked;
+      });
+    }
+
+    // if all the checkboxes are checked, check the checkAll
+    const groupChecked = newGroup.filter(
+      (checkbox) => checkbox.checked && !checkbox.checkAll
+    );
+    if (
+      groupChecked.length === newGroup.length - 1 &&
+      newGroup.find((checkbox) => checkbox.checkAll)
+    ) {
+      newGroup.forEach((checkbox) => {
+        checkbox.checked = true;
+      });
+    }
+
+    // if there is at least one checkbox unchecked, uncheck the checkAll
+    const groupUnchecked = newGroup.filter(
+      (checkbox) => !checkbox.checked && !checkbox.checkAll
+    );
+    if (
+      groupUnchecked.length > 0 &&
+      newGroup.find((checkbox) => checkbox.checkAll)
+    ) {
+      const checkAllIndex = newGroup.findIndex((checkbox) => checkbox.checkAll);
+      if (checkAllIndex !== -1) {
+        newGroup[checkAllIndex].checked = false;
+      }
+    }
+
+    // get the values of the checked
+    const values = newGroup
+      .filter((checkbox) => checkbox.checked)
+      .map((checkbox) => checkbox.value);
+
+    // set the new group of checkboxes and call the onChange function
+    setCheckboxes(newGroup);
+    onChange(values);
+  };
+
+  useEffect(() => {
+    const groupAllChecked = group.filter((checkbox) => checkbox.checkAll);
+    if (groupAllChecked.length > 1) {
+      throw new Error("Only one checkbox can be checkAll");
+    }
+  }, [group]);
+
+  return (
+    <div className={className}>
+      {checkboxes.map((checkbox, index) =>
+        checkbox.checkAll ? (
+          <div key={index} className="checkbox-checkAll">
+            <input
+              type="checkbox"
+              value={checkbox.value.toString()}
+              checked={checkbox.checked}
+              onChange={() => handleChange(index)}
+            />
+            <label htmlFor={checkbox.value.toString()}>{checkbox.label}</label>
+          </div>
+        ) : (
+          <div key={index}>
+            <input
+              type="checkbox"
+              value={checkbox.value.toString()}
+              checked={checkbox.checked}
+              onChange={() => handleChange(index)}
+            />
+            <label htmlFor={checkbox.value.toString()}>{checkbox.label}</label>
+          </div>
+        )
+      )}
+    </div>
+  );
+}
+
+export default Checkbox;
