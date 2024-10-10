672ea084eb625d7ad8ea381ca410a6132fbfc0ef
Alexandre <116552630+Creepynax@users.noreply.github.com>
2024-10-10 10:31:40 +0200

    Select + General CSS
    Ajout composant select + l'ajout du style général du site

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import './App.css';
+import './components/assets/styleGeneral.css';
+import SelectExample from './components/Select/Select.example';
 
 function App() {
 
   return (
     <>
-      
+      <SelectExample/>
     </>
   )
 }
 
-export default App
+export default App;
