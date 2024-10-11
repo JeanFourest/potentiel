b351d5eb16bc8a97647070c08b7513ed498323da
Alexandre <116552630+Creepynax@users.noreply.github.com>
2024-10-11 12:01:18 +0200

    ajout coverage + accordeon complet
    Ajout du coverage pour les tests + ajout de l'accordeon au complet + maj des exemples de code, des tests et du readme

--- src/components/checkbox/Checkbox.example.tsx
+++ src/components/checkbox/Checkbox.example.tsx
@@ -6,55 +6,29 @@ function CheckboxExample() {
   };
 
   const codeExample1 = `
-  <div class="checkbox-sm">
-
-    <div class="checkbox-checkAll">
-      <input type="checkbox" value="all" />
-      <label for="all">All</label>
-    </div>
-
-    <div>
-      <input type="checkbox" value="1" />
-      <label for="1">option 1</label>
-    </div>
-
-    <div>
-      <input type="checkbox" value="2" />
-      <label for="2">option 2</label>
-    </div>
-
-    <div>
-      <input type="checkbox" value="3" />
-      <label for="3">option 3</label>
-    </div>
-    
-  </div>
+  <Checkbox
+    group={[
+      { label: "All", value: "all", checked: false, checkAll: true },
+      { label: "option 1", value: 1, checked: false, checkAll: false },
+      { label: "option 2", value: 2, checked: false, checkAll: false },
+      { label: "option 3", value: 3, checked: false, checkAll: false },
+    ]}
+    onChange=...
+    className="checkbox-sm"
+  />
     `;
 
   const codeExample2 = `
-  <div class="checkbox-lg">
-
-    <div>
-      <input type="checkbox" value="1" />
-      <label for="1">option 1</label>
-    </div>
-
-    <div>
-      <input type="checkbox" value="2" />
-      <label for="2">option 2</label>
-    </div>
-
-    <div>
-      <input type="checkbox" value="3" />
-      <label for="3">option 3</label>
-    </div>
-
-    <div>
-      <input type="checkbox" value="4" />
-      <label for="4">option 4</label>
-    </div>
-    
-  </div>
+  <Checkbox
+    group={[
+      { label: "option 1", value: 1, checked: false, checkAll: false },
+      { label: "option 2", value: 2, checked: false, checkAll: false },
+      { label: "option 3", value: 3, checked: false, checkAll: false },
+      { label: "option 4", value: 4, checked: false, checkAll: false },
+    ]}
+    onChange=...
+    className="checkbox-lg"
+  />
     `;
 
   return (
